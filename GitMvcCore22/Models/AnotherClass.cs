398585d92381b596343122372021f116c4329618
Håkan Kilström <hakan.kilstrom@outlook.com>
2019-01-23 12:07:14 +0100

    changed prop name

--- GitMvcCore22/Models/AnotherClass.cs
+++ GitMvcCore22/Models/AnotherClass.cs
@@ -10,7 +10,7 @@ namespace GitMvcCore22.Models
         List<int> list = new List<int>();
         public string newString { get; set; }
         public DateTime MyProperty { get; set; }
-        public int number { get; set; }
+        public int Capacity { get; set; }
         public int int6 { get; set; }
         public string anotherString { get; set; }
     }

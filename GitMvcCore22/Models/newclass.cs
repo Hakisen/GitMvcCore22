835e25b6790e201e6f07c0ec90bcc2a085b9f66a
mina fathi <fathimina64@gmail.com>
2019-01-23 10:32:42 +0100

    new class added

---
+++ GitMvcCore22/Models/newclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitMvcCore22.Models
+{
+    public class newclass
+    {
+        public string Name { get; set; }
+    }
+}

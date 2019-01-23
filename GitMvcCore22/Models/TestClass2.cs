273b6579f233c7cfb76553a7e66d7aa01506847f
mina fathi <fathimina64@gmail.com>
2019-01-23 09:58:39 +0100

    datetipe property

--- GitMvcCore22/Models/TestClass2.cs
+++ GitMvcCore22/Models/TestClass2.cs
@@ -9,7 +9,7 @@ namespace GitMvcCore22.Models
     {
         //Tillagt av Håkan efter 2:a gemensamma synken
         public int OldProperty { get; set; }
-        public string StringProp { get; set; }
+        public DateTime StringProp { get; set; }
         public string StringProp2 { get; set; }
     }
 }

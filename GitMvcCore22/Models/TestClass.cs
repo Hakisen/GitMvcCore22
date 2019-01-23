517574fee8a94cc81b83df3d11177c61c12a4953
Michael Kender <michael.kender@telia.com>
2019-01-23 09:29:51 +0100

    - moved TestClass  to Models folder - Faderittan property added to class

---
+++ GitMvcCore22/Models/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitMvcCore22
+{
+    public class TestClass
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public DateTime Orderdate { get; set; }
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+        public string Faderittan { get; set; }
+    }
+}
